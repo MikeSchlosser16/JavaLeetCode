8ef88b80d2449acb5a7118e927c0a2ab7121ecd4
Mike Schlosser <schlo201@d.umn.edu>
2018-01-05 17:08:57 -0600

    Adding JudgeRouteCircle

--- ArrayPartition1.java
+++ ArrayPartition1.java
@@ -16,7 +16,6 @@ class Solution {
         int total = 0;
         for(int i = 0; i < nums.length; i += 2){
             total += Math.min(nums[i], nums[i + 1]);
-            i += 2;
         }
         return total;
     }

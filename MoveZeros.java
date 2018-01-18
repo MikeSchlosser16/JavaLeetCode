a973ce9dec7d7a1c231e221be9cc8de64a6e329f
Mike Schlosser <schlo201@d.umn.edu>
2018-01-17 23:13:14 -0600

    Updated with cleaner solution

--- MoveZeros.java
+++ MoveZeros.java
@@ -25,3 +25,18 @@ class Solution {
         
     }
 }
+
+
+// Cleaner code
+public void moveZeroes(int[] nums) {
+    if (nums == null || nums.length == 0) return;        
+
+    int insertPos = 0;
+    for (int num: nums) {
+        if (num != 0) nums[insertPos++] = num;
+    }        
+
+    while (insertPos < nums.length) {
+        nums[insertPos++] = 0;
+    }
+}

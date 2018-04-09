19e38ea457a48a70e16c497f0dd422f2c7b5ac14
Mike Schlosser <schlo201@d.umn.edu>
2018-04-08 22:40:14 -0500

    Added one more clever solution

--- Easy/ReverseString.java
+++ Easy/ReverseString.java
@@ -21,3 +21,20 @@ class Solution {
     return new StringBuilder(s).reverse().toString();
   }
 }
+
+
+// Swap throgh 'half' of string
+public class Solution {
+    public String reverseString(String s) {
+        char[] word = s.toCharArray();
+        int i = 0;
+        int j = s.length() - 1;
+        while (i < j) {
+            char temp = word[i];
+            word[i] = word[j];
+            word[j] = temp;
+            i++;
+            j--;
+        }
+        return new String(word);
+    }

de75e55a7908d18dc51e6d7d4b66721f7151773b
Mike Schlosser <schlo201@d.umn.edu>
2018-04-19 22:58:10 -0500

    Add dist candies

---
+++ Easy/DistributeCandies.java
@@ -0,0 +1,21 @@
+/*Given an integer array with even length, where different numbers in this array represent different kinds of candies. Each number means one candy of the corresponding kind. You need to distribute these candies equally in number to brother and sister. Return the maximum number of kinds of candies the sister could gain.
+Example 1:
+Input: candies = [1,1,2,2,3,3]
+Output: 3
+Explanation:
+There are three different kinds of candies (1, 2 and 3), and two candies for each kind.
+Optimal distribution: The sister has candies [1,2,3] and the brother has candies [1,2,3], too.
+The sister has three different kinds of candies. */
+
+class Solution {
+    public int distributeCandies(int[] candies) {
+        // No duplicates in set
+        Set<Integer> candySet = new HashSet<>();
+
+        for(int candy : candies)
+            candySet.add(candy);
+        // The sister can get at most half the candies, of the number of unique candies. Which ever is lower is the answer
+        return Math.min(candySet.size(), candies.length / 2);
+
+    }
+}
